614b98e57eaa8f80c88cc1c701ad2b1f9b452bbf
Aparna Warade <aparna.warade@outlook.com>
2024-01-29 16:07:47 -0500

    Update choice_method.py

--- NumpyExamples/choice_method.py
+++ NumpyExamples/choice_method.py
@@ -12,3 +12,18 @@ print(x)
 
 x=random.choice([3,7,9,12],size=(5))
 print(x)
+
+"""
+Generate a 1-D array containing 100 values, where each value has to be 3, 5, 7 or 9.
+
+The probability for the value to be 3 is set to be 0.1
+
+The probability for the value to be 5 is set to be 0.3
+
+The probability for the value to be 7 is set to be 0.6
+
+The probability for the value to be 9 is set to be 0
+"""
+x=random.choice([3,5,7,9],p=[0.1,0.3,0.6,0.0],size=(100))
+print("----1D array with choice fiunction and probability with size 100 ----")
+print(x)
